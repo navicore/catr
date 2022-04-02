1a367636f98e84dfb701b68b2949cea39d762dab
Ed Sweeney <ed@onextent.com>
2022-04-01 20:16:04 -0700

    print

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,20 @@
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 #[derive(Debug)]
 pub struct Config {
     files: Vec<String>,
     number_lines: bool,
     number_nonblank_lines: bool,
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
 
 pub fn get_args() -> MyResult<Config> {
@@ -46,7 +55,29 @@ type MyResult<T> = Result<T, Box<dyn Error>>;
 
 pub fn run(config: Config) -> MyResult<()> {
     for filename in config.files {
-        println!("{}", filename);
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to print {}: {}", filename, err),
+            Ok(f) => {
+                let mut number = 0;
+                for line in f.lines() {
+                    match line {
+                        Err(err) => eprintln!("Failed to print line: {}", err),
+                        Ok(l) => {
+                            if config.number_nonblank_lines && l.is_empty() {
+                                continue;
+                            };
+                            number = number + 1;
+                            let mut number_str = String::from("");
+                            if config.number_lines || config.number_nonblank_lines {
+                                number_str = number.to_string();
+                                number_str.push(' ');
+                            };
+                            println!("{}{}", number_str, l)
+                        }
+                    }
+                }
+            }
+        }
     }
     Ok(())
 }
